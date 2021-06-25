6cda1ddda37465477ec4701305809866e03209dd
Heberto Rosillo <heberto.rosillo.c@gmail.com>
2021-06-25 00:28:57 -0500

    server cloud config
    server cloud config

---
+++ src/test/java/com/everis/servercloudconfig/ServercloudconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.servercloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServercloudconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
